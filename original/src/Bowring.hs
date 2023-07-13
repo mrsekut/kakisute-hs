f54e3e1c479d2dd3e90901c2b8e5b47eed8b9c26
mrsekut <k.cloudspider@gmail.com>
2023-07-13 22:17:09 +0900

    clean: framesScore

--- original/src/Bowring.hs
+++ original/src/Bowring.hs
@@ -10,7 +10,6 @@ data BowlingError = IncompleteGame
 
 
 type Roll = Int
-type NextRoll = Roll
 
 data Frame
   = Normal NormalFrame
@@ -39,14 +38,14 @@ score rolls = do
     b <- reverse <$> a
     c <- mapM unwrapPendingFrame b
     d <- validateFrameCount c
-    pure $ score' d
+    pure $ framesScore d
 -- TODO: clean
 
 
 -- TODO: やっぱこれが微妙なんだよなあ..
 -- TODO: 書き換え案1: Stateモナド
 data PendingFrame
-  = Done CommonFrame -- TODO: ここのFrame参照されてない？
+  = Done CommonFrame
   | Pend [Roll] -- TODO: ここも厳密でない。Normalだと0/1要素で、ラストだけ1/2
   deriving (Show)
 
@@ -69,6 +68,7 @@ roll ps@(last:tail) (idx,roll)
       -- 10 frame目の1投目
       r <- validateRoll1 (idx,roll)
       pure $ Pend [r] : ps
+
   | length ps == 10 = case last of
       Done _ -> Left $ InvalidRoll idx roll -- lastが0 0 0
       Pend [p]   -> if p + roll < 10
@@ -86,7 +86,6 @@ roll ps@(last:tail) (idx,roll)
           rs <- validateRoll3 p q (idx,r)
           pure $ Done (LastC rs) : tail
 
-    --  Pend [p,q] -> pure $ Done (LastC [p,q,roll]) : tail
   | otherwise = do
     -- TODO: clean
     case last of
@@ -104,7 +103,6 @@ mkFrame1 (idx,r1)
   | r1 == 10           = pure $ Done StrikeC
   | 0 <= r1 && r1 < 10 = pure $ Pend [r1]
   | otherwise          = Left $ InvalidRoll idx r1
-
 
 
 -- TODO: where? validateRoll2を使って書けない？
@@ -127,19 +125,32 @@ unwrapPendingFrame (Done (LastC [r1,r2,r3])) = pure $ Last $ Thr (r1,r2,r3)
 unwrapPendingFrame (Pend _)                  = Left IncompleteGame
 
 
--- TODO: name
-score' :: [Frame] -> Int
-score' []                            = 0
-score' ((Normal (Open (r1, r2))):fs) = r1 + r2 + score' fs
-score' ((Normal (Spare r)):fs)       = 10 + getNext1 fs + score' fs
-score' ((Normal Strike):fs)          = 10 + getNext2 fs + score' fs
-score' (Last (Two (r1,r2)):fs)       = r1 + r2 + score' fs
-score' ((Last (Thr (r1,r2,r3))):fs)  = r1 + r2 + r3 + score' fs
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+
+framesScore :: [Frame] -> Int
+framesScore []     = 0
+framesScore (x:xs) = frameScore (x:xs) + framesScore xs
+  where
+    frameScore ((Normal (Open (r1, r2))):_) = r1 + r2
+    frameScore ((Normal (Spare _)):xs)      = 10 + getNext1 xs
+    frameScore ((Normal Strike):xs)         = 10 + getNext2 xs
+    frameScore (Last (Two (r1,r2)):_)       = r1 + r2
+    frameScore ((Last (Thr (r1,r2,r3))):_)  = r1 + r2 + r3
 
 
 getNext1 :: [Frame] -> Int
 getNext1 [] = 0
-getNext1 (f:_) = get f
+getNext1 (x:_) = get x
   where
     get :: Frame -> Int
     get (Normal (Open (n1,_))) = n1
@@ -150,12 +161,13 @@ getNext1 (f:_) = get f
 
 
 getNext2 :: [Frame] -> Int
-getNext2 (f:fs) = get f
+getNext2 []     = 0
+getNext2 (x:xs) = get x
   where
     get :: Frame -> Int
     get (Normal (Open (n1,n2))) = n1 + n2
-    get (Normal (Spare n1))     = 10
-    get (Normal Strike)         = 10 + getNext1 fs
+    get (Normal (Spare _))      = 10
+    get (Normal Strike)         = 10 + getNext1 xs
     get (Last (Two (n1,n2)))    = n1 + n2
     get (Last (Thr (n1,n2,_)))  = n1 + n2
 
@@ -207,130 +219,3 @@ validateFrameCount fs
 
 indexedFoldM :: Monad m => (a -> (Int, b) -> m a) -> a -> [b] -> m a
 indexedFoldM f acc xs = foldM f acc (zip [0..] xs)
-
-
-
-
-
-
-
--- cases = [ Case { description = "should be able to score a game with all zeros"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 0
---                }
---         , Case { description = "should be able to score a game with no strikes or spares"
---                , rolls       = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
---                , expected    = Right 90
---                }
---         , Case { description = "a spare followed by zeros is worth ten points"
---                , rolls       = [6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 10
---                }
---         , Case { description = "points scored in the roll after a spare are counted twice"
---                , rolls       = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 16
---                }
---         , Case { description = "consecutive spares each get a one roll bonus"
---                , rolls       = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 31
---                }
---         , Case { description = "a spare in the last frame gets a one roll bonus that is counted once"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7]
---                , expected    = Right 17
---                }
---         , Case { description = "a strike earns ten points in a frame with a single roll"
---                , rolls       = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 10
---                }
---         , Case { description = "points scored in the two rolls after a strike are counted twice as a bonus"
---                , rolls       = [10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 26
---                }
---         , Case { description = "consecutive strikes each get the two roll bonus"
---                , rolls       = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Right 81
---                }
---         , Case { description = "a strike in the last frame gets a two roll bonus that is counted once"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1]
---                , expected    = Right 18
---                }
---         , Case { description = "rolling a spare with the two roll bonus does not get a bonus roll"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3]
---                , expected    = Right 20
---                }
---         , Case { description = "strikes with the two roll bonus do not get bonus rolls"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]
---                , expected    = Right 30
---                }
---         , Case { description = "a strike with the one roll bonus after a spare in the last frame does not get a bonus"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10]
---                , expected    = Right 20
---                }
---         , Case { description = "all strikes is a perfect game"
---                , rolls       = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
---                , expected    = Right 300
---                }
---         , Case { description = "rolls cannot score negative points"
---                , rolls       = [-1]
---                , expected    = Left $ InvalidRoll 0 (-1)
---                }
---         , Case { description = "a roll cannot score more than 10 points"
---                , rolls       = [11]
---                , expected    = Left $ InvalidRoll 0 11
---                }
---         , Case { description = "two rolls in a frame cannot score more than 10 points"
---                , rolls       = [5, 6]
---                , expected    = Left $ InvalidRoll 1 6
---                }
---         , Case { description = "bonus roll after a strike in the last frame cannot score more than 10 points"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 11]
---                , expected    = Left $ InvalidRoll 19 11
---                }
---         , Case { description = "two bonus rolls after a strike in the last frame cannot score more than 10 points"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 5, 6]
---                , expected    = Left $ InvalidRoll 20 6
---                }
---         , Case { description = "two bonus rolls after a strike in the last frame can score more than 10 points if one is a strike"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 6]
---                , expected    = Right 26
---                }
---         , Case { description = "the second bonus rolls after a strike in the last frame cannot be a strike if the first one is not a strike"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 6, 10]
---                , expected    = Left $ InvalidRoll 20 10
---                }
---         , Case { description = "second bonus roll after a strike in the last frame cannot score more than 10 points"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 11]
---                , expected    = Left $ InvalidRoll 20 11
---                }
---         , Case { description = "an unstarted game cannot be scored"
---                , rolls       = []
---                , expected    = Left IncompleteGame
---                }
---         , Case { description = "an incomplete game cannot be scored"
---                , rolls       = [0, 0]
---                , expected    = Left IncompleteGame
---                }
---         , Case { description = "cannot roll if game already has ten frames"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
---                , expected    = Left $ InvalidRoll 20 0
---                }
---         , Case { description = "bonus rolls for a strike in the last frame must be rolled before score can be calculated"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10]
---                , expected    = Left IncompleteGame
---                }
---         , Case { description = "both bonus rolls for a strike in the last frame must be rolled before score can be calculated"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10]
---                , expected    = Left IncompleteGame
---                }
---         , Case { description = "bonus roll for a spare in the last frame must be rolled before score can be calculated"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3]
---                , expected    = Left IncompleteGame
---                }
---         , Case { description = "cannot roll after bonus roll for spare"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 2, 2]
---                , expected    = Left $ InvalidRoll 21 2
---                }
---         , Case { description = "cannot roll after bonus rolls for strike"
---                , rolls       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 3, 2, 2]
---                , expected    = Left $ InvalidRoll 21 2
---                }
