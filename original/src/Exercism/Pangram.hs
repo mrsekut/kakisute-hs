9c3d41aabc21fc97aa6cccc0c4172abac916a9a6
mrsekut <k.cloudspider@gmail.com>
2023-07-20 22:03:59 +0900

    Pangram

---
+++ original/src/Exercism/Pangram.hs
@@ -0,0 +1,7 @@
+-- https://exercism.org/tracks/haskell/exercises/pangram
+module Pangram (isPangram) where
+import           Data.Char (toLower)
+
+
+isPangram :: String -> Bool
+isPangram text = all (`elem` map toLower text) ['a'..'z']
