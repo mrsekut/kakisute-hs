d606c4310286ccb7c0e4b8dfb1c9674b5e44c531
mrsekut <k.cloudspider@gmail.com>
2023-07-30 14:51:55 +0900

    refactor

--- original/src/Exercism/Bob.hs
+++ original/src/Exercism/Bob.hs
@@ -8,23 +8,15 @@ import qualified Data.Text as T
 
 responseFor :: Text -> Text
 responseFor xs
-  | isSlience xs                   = "Fine. Be that way!"
-  | isShouting xs && isQuestion xs = "Calm down, I know what I'm doing!"
-  | isQuestion xs                  = "Sure."
-  | isShouting xs                  = "Whoa, chill out!"
-  | otherwise                      = "Whatever."
-
-
-isSlience :: Text -> Bool
-isSlience xs = T.length (T.strip xs) == 0
-
-
-isQuestion :: Text -> Bool
-isQuestion = T.isSuffixOf "?" . T.strip
-
-
-isShouting :: Text -> Bool
-isShouting xs = hasLetters xs && isAllUpper xs
+  | silience             = "Fine. Be that way!"
+  | shouting && question = "Calm down, I know what I'm doing!"
+  | question             = "Sure."
+  | shouting             = "Whoa, chill out!"
+  | otherwise            = "Whatever."
   where
-    hasLetters = any isAlpha . T.unpack
-    isAllUpper xs = T.toUpper xs == xs
+    silence = T.length (T.strip xs) == 0
+    question = T.isSuffixOf "?" . T.strip $ xs
+    shouting = hasLetters xs && isAllUpper xs
+      where
+        hasLetters = any isAlpha . T.unpack
+        isAllUpper xs = T.toUpper xs == xs
