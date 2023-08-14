c14209e4b8020a35405149ccda7f86d5bc217832
mrsekut <k.cloudspider@gmail.com>
2023-08-14 15:04:08 +0900

    with deriving read

--- src/Exercism/NucleotideCount.hs
+++ src/Exercism/NucleotideCount.hs
@@ -1,18 +1,18 @@
-module Exercism.NucleotideCount (nucleotideCounts, Nucleotide(..)) where
+module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import           Data.Map (Map, fromListWith)
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = fromList <$> traverse mkNucleotide xs
+nucleotideCounts xs = fromList <$> mkNucleotides xs
 
 fromList :: [Nucleotide] -> Map Nucleotide Int
 fromList ns = fromListWith (+) $ zip ns [1,1..]
 
-mkNucleotide :: Char -> Either String Nucleotide
-mkNucleotide 'A' = Right A
-mkNucleotide 'C' = Right C
-mkNucleotide 'G' = Right G
-mkNucleotide 'T' = Right T
-mkNucleotide _   = Left "invalid"
+mkNucleotides :: String -> Either String [Nucleotide]
+mkNucleotides s
+  | isValid s = Right $ map (read . (: [])) s
+  | otherwise = Left "invalid"
+  where
+    isValid = all (`elem` "ACGT")
