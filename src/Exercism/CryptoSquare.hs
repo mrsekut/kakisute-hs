0147fe7ae8a48a5559e1083b581a38595a1377a2
mrsekut <k.cloudspider@gmail.com>
2023-08-27 16:43:12 +0900

    refactor

--- src/Exercism/CryptoSquare.hs
+++ src/Exercism/CryptoSquare.hs
@@ -1,31 +1,19 @@
 module CryptoSquare (encode) where
 import           Data.Char (isAlphaNum, isSpace, toLower)
+import           Data.List (transpose)
 
 encode :: String -> String
-encode xs = unwords . zipR $ chunk c ys
+encode = unwords . transpose . split . prettify
   where
-    ys = prettify xs
-    c = col $ length ys
+    prettify :: String -> String
+    prettify = map toLower . filter isAlphaNum
 
-zipR :: [String] -> [String]
-zipR [] = []
-zipR xs
-  | all null xs = []
-  | otherwise   = concatMap head' xs : zipR (map tail' xs)
-  where
-    head' ""     = " "
-    head' (s:ss) = [s]
+    split :: String -> [String]
+    split xs = chunksOf (col xs) xs
 
-    tail' ""     = ""
-    tail' (s:ss) = ss
+    col :: String -> Int
+    col = ceiling . sqrt . fromIntegral . length
 
-prettify :: String -> String
-prettify = map toLower . filter isAlphaNum
-
-col :: Int -> Int
-col l = head [c | c <- [1..], r <- [c-1,c], r * c >= l]
-
-chunk :: Int -> String -> [String]
-chunk n "" = []
-chunk n xs = take n xs : chunk n (drop n xs)
-
+    chunksOf :: Int -> String -> [String]
+    chunksOf n "" = []
+    chunksOf n xs = take n (xs ++ repeat ' ') : chunksOf n (drop n xs)
