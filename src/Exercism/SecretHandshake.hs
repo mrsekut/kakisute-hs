5b209bd4b51e24d0124cf02b2f68b31e0b852c48
mrsekut <k.cloudspider@gmail.com>
2023-11-03 22:56:45 +0900

    secret handshake

---
+++ src/Exercism/SecretHandshake.hs
@@ -0,0 +1,15 @@
+module SecretHandshake where
+
+handshake :: Int -> [String]
+handshake = toCommand . toRevBins
+  where
+    toRevBins 0 = []
+    toRevBins n = (n `mod` 2) : toRevBins (n `div` 2)
+
+    toCommand xs@[1,_,_,_,_] = reverse . commands $ xs
+    toCommand xs             = commands xs
+
+    commands xs = [action | (bit, action) <- zip xs actions, bit == 1]
+
+    actions = ["wink","double blink","close your eyes","jump"]
+
