842431365ad8cfed2bf7e80f1af10a75b9c23e7f
mrsekut <k.cloudspider@gmail.com>
2024-12-25 22:42:26 +0900

    GameOfLife

--- kakisute/src/Exercism/GameOfLife.hs
+++ kakisute/src/Exercism/GameOfLife.hs
@@ -1,36 +1,32 @@
 module GameOfLife (tick) where
-import           Data.List (transpose)
+import           Data.List (tails, transpose)
 
 tick :: [[Int]] -> [[Int]]
-tick xss = map (map applyRule) $ zipWith zip xss neighbors
+tick xss = map (map conway) $ zipWith zip xss neighbors
   where
-    neighbors = zipWith (zipWith (-)) (sumNeighbors xss) xss
+    neighbors = zipWith (zipWith (-)) (neighborhoodSum xss) xss
 
-
--- セルとその周囲のセルの和
-sumNeighbors :: [[Int]] -> [[Int]]
-sumNeighbors = colSum . rowSum
-  where
-    colSum = map sumAdjacent
-    rowSum = transpose . map sumAdjacent . transpose
-
-
-sumAdjacent :: [Int] -> [Int]
-sumAdjacent = slidingSum . expand
-  where
-    slidingSum (a:b:c:rs) = (a+b+c) : slidingSum (b:c:rs)
-    slidingSum _          = []
-
-
-expand :: [Int] -> [Int]
-expand xs = 0 : xs ++ [0]
-
-
-applyRule :: (Int,Int) -> Int
-applyRule (0,n)
+conway :: (Int,Int) -> Int
+conway (0,n)
   | n == 3    = 1
   | otherwise = 0
-applyRule (1,n)
+conway (1,n)
   | n == 2    = 1
   | n == 3    = 1
   | otherwise = 0
+
+
+neighborhoodSum :: [[Int]] -> [[Int]]
+neighborhoodSum = rowSum . colSum
+  where
+    colSum = map windows3Sum
+    rowSum = transpose . map  windows3Sum. transpose
+
+windows3Sum :: [Int] -> [Int]
+windows3Sum = map sum . windows3 . padding
+
+windows3 :: [a] -> [[a]]
+windows3 xs = [ [x, y, z] | (x:y:z:_) <- tails xs ]
+
+padding :: [Int] -> [Int]
+padding xs = 0 : xs ++ [0]
