4beabe53cdc5ac91a0f0c3434522477f6825eb2a
mrsekut <k.cloudspider@gmail.com>
2024-12-29 18:36:35 +0900

    Raindrops

--- kakisute/src/Exercism/Raindrops.hs
+++ kakisute/src/Exercism/Raindrops.hs
@@ -1,27 +1,9 @@
 module Raindrops (convert) where
 
--- TODO: text
-
--- >>> convert 28
--- >>> convert 30
--- >>> convert 34
--- "Plong"
--- "PlingPlang"
--- "34"
 convert :: Int -> String
-convert n = if a == "" then show n else a
+convert n
+  | ppp == "" = show n
+  | otherwise = ppp
   where
-    a = foldl (\acc f -> acc ++ f n) "" [f3, f5, f7]
-
-
-f3 :: Int -> String
-f3 n = if n `mod` 3 == 0 then "Pling" else ""
-
-f5 :: Int -> String
-f5 n = if n `mod` 5 == 0 then "Plang" else ""
-
-f7 :: Int -> String
-f7 n = if n `mod` 7 == 0 then "Plong" else ""
-
-other :: Int -> String
-other = show
+    ppp = concatMap (uncurry f) [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+    f x s = if n `mod` x == 0 then s else ""
