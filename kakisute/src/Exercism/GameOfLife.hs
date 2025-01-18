cb8fe21e29112a7b57a1d552b4f3ebb3e5a2c71c
mrsekut <k.cloudspider@gmail.com>
2025-01-18 11:00:25 +0900

    clean

--- kakisute/src/Exercism/GameOfLife.hs
+++ kakisute/src/Exercism/GameOfLife.hs
@@ -15,30 +15,18 @@ conway (1,n)
   | n == 3    = 1
   | otherwise = 0
 
-
--- >>> neighborhoodSum [[1,2,3],[4,5,6],[7,8,9]]
--- [[12,21,16],[27,45,33],[24,39,28]]
 neighborhoodSum :: [[Int]] -> [[Int]]
 neighborhoodSum = map smoothCols . smoothRows
   where
-    smoothCols = trips add3 0
-    smoothRows = trips (zipWith3 add3) (repeat 0)
+    smoothCols = map (\[a,b,c] -> add3 a b c) . windows3 . padding 0
+    smoothRows = map (\[a,b,c] -> zipWith3 add3 a b c) . windows3 . padding [0,0..]
+    -- smoothRows = transpose . map smoothCols . transpose
 
--- 1次元リストの各要素について、前後3つを取り出して関数を適用。
--- >>> trips add3 0 [1,2,3,4,5]
--- [3,6,9,12,9]
-trips :: (a -> a -> a -> b) -> a -> [a] -> [b]
-trips f border = map (\[a,b,c] -> f a b c) . windows3 . padding border
-
--- >>> padding 0 [1,2,3]
--- [0,1,2,3,0]
 padding :: a -> [a] -> [a]
 padding border xs = border : xs ++ [border]
 
--- >>> windows3 [1,2,3,4,5]
--- [[1,2,3],[2,3,4],[3,4,5]]
 windows3 :: [a] -> [[a]]
-windows3 = filter (\x -> length x >= 3) . map (take 3) . tails
+windows3 xs = [ [x, y, z] | (x:y:z:_) <- tails xs ]
 
 add3 :: Int -> Int -> Int -> Int
 add3 a b c = a + b + c
